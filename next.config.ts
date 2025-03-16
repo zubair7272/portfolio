c19bf0d7c0c7a2ad018570a97ad61f7072075148
zubair7272 <nisarzubair414@gmail.com>
2025-03-16 16:32:02 +0530

    new build

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,10 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  output: 'standalone',
+  experimental: {
+    serverComponentsExternalPackages: ['mongoose']
+  },
 };
 
 module.exports = nextConfig;
